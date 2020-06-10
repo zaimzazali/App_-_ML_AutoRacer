9e9e30ce39d3c3f6bd2f05b07146e750044b7fde
Zaim Zazali <zaim.zazali@gmail.com>
2020-06-10 23:26:59 +0100

    Updated Icons and Texts

---
+++ apps/Assets/Scripts/tileEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileEvents : MonoBehaviour
+{
+    public float expandSize = 0.1f;
+    public float expandDuration = 0.1f;
+    
+    public void tileExpand() {
+        LeanTween.scale(gameObject, new Vector3(expandSize,expandSize,expandSize), expandDuration);
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    public void tileNormal() {
+        LeanTween.scale(gameObject, new Vector3(1f,1f,1f), expandDuration);
+        // gameObject.GetComponent<AudioSource>().Stop();
+    }
+}
