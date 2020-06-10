efd8bffe599a63db65d8893aeddded5bd9a30264
Zaim Zazali <zaim.zazali@gmail.com>
2020-06-10 15:08:30 +0100

    Update login scene

---
+++ apps/Assets/Scripts/initSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class initSound : MonoBehaviour
+{
+    public float waitingTime = 0;
+    private AudioSource thisSound;
+
+    private void Start()
+    {
+        thisSound = GetComponent<AudioSource>();
+        playSound();
+    }
+
+    private void playSound()
+    {
+        thisSound.PlayDelayed(waitingTime);
+    }
+}
