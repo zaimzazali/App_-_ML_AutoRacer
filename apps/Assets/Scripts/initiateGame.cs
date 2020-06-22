f9a807033ea8dcb506d3987a847f5f7625f4a69c
Zaim Zazali <zaim.zazali@gmail.com>
2020-06-22 11:21:57 +0100

    Updated intro scene

--- apps/Assets/Scripts/initiateGame.cs
+++ apps/Assets/Scripts/initiateGame.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class initiateGame : MonoBehaviour
 {
     private double videoLength = 0.0;
+    private bool keyPressed = false;
 
     private void Awake() {
         videoLength = GameObject.Find("Video Player").gameObject.GetComponent<VideoPlayer>().length;
@@ -14,8 +15,22 @@ public class initiateGame : MonoBehaviour
         PlayerPrefs.SetInt("nextSceneIndex", 0);
     }
 
+    private void Update() {
+        StartCoroutine("toSkipCutscene");
+    }
+
     private IEnumerator waitForVideo() {
         yield return new WaitForSeconds((float)videoLength);
         SceneManager.LoadScene(1);
+    }
+
+    private IEnumerator toSkipCutscene() {
+        if (!keyPressed && Input.anyKey) {
+            keyPressed = true;
+            AsyncOperation gameLevel = SceneManager.LoadSceneAsync(1);
+            while (gameLevel.progress < 1f) {
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 }
