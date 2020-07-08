013407747ebd807dc8d2573837ca860960a54dfb
Zaim Zazali <zaim.zazali@gmail.com>
2020-07-08 17:20:07 +0100

    Updated

---
+++ apps/unity/Assets/Scripts/Racing/cameraManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject focus = null;
+    [SerializeField]
+    private float distance = 0f;
+    [SerializeField]
+    private float height = 0f;
+    [SerializeField]
+    private float dampening = 0f;
+
+    private void Update() {
+        transform.position = Vector3.Lerp(transform.position, focus.transform.position + focus.transform.TransformDirection(new Vector3(0f, height, -distance)), dampening * Time.deltaTime);
+        transform.LookAt(focus.transform);
+    }
+}
---
+++ apps/unity/Assets/Scripts/Racing/inputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputManager : MonoBehaviour
+{
+    [SerializeField]
+    private float throttle = 0f;
+    [SerializeField]
+    private float steer = 0f;
+
+    // Update is called once per frame
+    private void Update() {
+        throttle = Input.GetAxis("Vertical");
+        steer = Input.GetAxis("Horizontal");
+    }
+
+    public float getThrottle() {
+        return throttle;
+    }
+
+    public float getSteer() {
+        return steer;
+    }
+}
