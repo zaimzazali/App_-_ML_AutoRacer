8a4ff78f379219a97d2e6a49ca326805e8bf224a
Zaim Zazali <zaim.zazali@gmail.com>
2020-08-19 09:35:19 +0100

    Updated

--- release/trainer/build/python-lua/rewardFunction.lua
+++ release/trainer/build/python-lua/rewardFunction.lua
@@ -3,10 +3,10 @@ if passedCheckpoint then
     reward = (reward + 1.0)
 end
 if isCarReversed then
-    reward = (reward - 1.0)
+    reward = (reward - 5.0)
 else
-    reward = (reward + (distanceToCheckpoint * 0.03))
     reward = (reward + (speed * 0.02))
+    reward = (reward + (distanceToCheckpoint * 0.03))
 end
 if (speed > 0.0) then
     reward = (reward + 0.05)
@@ -15,18 +15,18 @@ local count = 0
 for index, value in ipairs(distanceToWall) do
     if ((count == 0) or (count == 2)) then
         if (value < 0.25) then
-            reward = (reward - 1.0)
+            reward = (reward - 5.0)
         end
     end
     if ((count == 1) or (count == 5)) then
         if (value < 0.1) then
-            reward = (reward - 1.0)
+            reward = (reward - 5.0)
         end
     end
     count = (count + 1)
     ::loop_label_1::
 end
 if isOutBound then
-    reward = (reward - 1.0)
+    reward = (reward - 10.0)
 end
 return reward
